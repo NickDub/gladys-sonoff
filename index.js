cc733631f04db08681121e5731a7d173774f6ce7
Dubois Nicolas <dubois.nicolas@free.fr>
2019-06-24 22:07:49 +0200

    feat: add init on Gladys start

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 module.exports = function (sails) {
 	const install = require('./lib/install');
+	const init = require('./lib/init');
 	const exec = require('./lib/exec');
+
+	gladys.on('ready', function() {
+		init();
+	});
 
 	return {
 		install: install,
+		init: init,
 		exec: exec
 	};
 };
